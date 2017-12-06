1802be626613e683573b44b3d551f0c8c7de59d4
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-12-06 01:25:42 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE projects (
+  title TEXT,
+  category TEXT,
+  goal INTEGER,
+  start_date INTEGER,
+  end_date INTEGER
+);
+
+CREATE TABLE users (
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledges (
+  amount INTEGER
+);
