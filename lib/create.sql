463c609cca78d96163641205625bd69fcaed549d
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-12-06 01:29:08 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,16 +1,21 @@
 CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
-  goal INTEGER,
-  start_date INTEGER,
-  end_date INTEGER
+  funding_goal NUMERIC,
+  start_date TEXT,
+  end_date TEXT
 );
 
 CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
 );
 
 CREATE TABLE pledges (
-  amount INTEGER
+  id INTEGER PRIMARY KEY,
+  amount NUMERIC,
+  user_id INTEGER,
+  project_id INTEGER
 );
